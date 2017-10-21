c0663833d8c13cab1eb7e36eb7bf52e3065b9495
Carlo Cirrincione <ccirrin@gmail.com>
2017-10-20 23:55:40 -0500

    Login Activity and Permission for Coarse Location

---
+++ app/src/main/java/org/vandyhacks/footprint/LoginActivity.java
@@ -0,0 +1,34 @@
+package org.vandyhacks.footprint;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameBox;
+    private Button usernameButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        usernameBox = (EditText) findViewById(R.id.usernameBox);
+        usernameButton = (Button) findViewById(R.id.usernameButton);
+
+        /*usernameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("username", usernameBox.getText().toString());
+                startActivity(intent);
+
+            }
+        });*/
+
+    }
+}
